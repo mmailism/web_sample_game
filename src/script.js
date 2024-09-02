0bfa273027a92396d11775f179199c4bab15c646
mailism <domiam09@gmail.com>
2024-09-02 15:40:01 +0700

    add-faq-fn

--- src/script.js
+++ src/script.js
@@ -17,3 +17,30 @@ window.onscroll = () =>{
         header.classList.remove('active');
     }
 }
+
+let faqBox = document.querySelectorAll('.faq .box-container .box');
+
+faqBox.forEach(box =>{
+    box.onclick = () =>{
+        let content = box.querySelector('.content');
+        let icon = box.querySelector('i');
+
+        if(content.classList.contains('active')){
+            content.classList.remove('active');
+        }else{
+            document.querySelectorAll('.faq .box-container .box .content').forEach(contents =>{
+                contents.classList.remove('active');
+            });
+            content.classList.add('active');
+        }
+
+        if(icon.classList.contains('fa-minus')){
+            icon.classList.replace('fa-minus', 'fa-plus');
+        }else{
+            document.querySelectorAll('.faq .box-container .box i').forEach(icons =>{
+                icons.classList.replace('fa-minus', 'fa-plus');
+            });
+            icon.classList.replace('fa-plus', 'fa-minus');
+        }
+    }
+})
